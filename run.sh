79161f1c338b976402dfda924e4e98dcf4796aa6
Jiasi Shen <jiasi@mit.edu>
2016-02-04 10:52:34 -0500

    Suppress JAVA_TOOL_OPTIONS message

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 gitroot=$(git rev-parse --show-toplevel)
-java -jar $gitroot/dist/Compiler.jar "$@"
+java_tool_options="$JAVA_TOOL_OPTIONS"
+unset JAVA_TOOL_OPTIONS
+java $java_tool_options -jar $gitroot/dist/Compiler.jar "$@"
