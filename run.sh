528769372705f164a56e36d249989e9d403239a0
Jiasi Shen <jiasi@mit.edu>
2016-02-04 13:49:14 -0500

    Supress better

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 gitroot=$(git rev-parse --show-toplevel)
-java_tool_options="$JAVA_TOOL_OPTIONS"
-unset JAVA_TOOL_OPTIONS
+if [ -n "$JAVA_TOOL_OPTIONS" ]; then
+  java_tool_options="$JAVA_TOOL_OPTIONS"
+  unset JAVA_TOOL_OPTIONS
+fi
 java $java_tool_options -jar $gitroot/dist/Compiler.jar "$@"
