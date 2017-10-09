091b9f4fe2913ff0a6d0053d0a3e59873b699c6e
Arkadiy Frasinich <arkadiyf@mit.edu>
2017-10-09 17:42:37 -0400

    We can now unit test!

---
+++ test/edu/mit/compilers/trees/ASTCreatorTest.java
@@ -0,0 +1,13 @@
+package edu.mit.compilers.trees;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ASTCreatorTest {
+    
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
