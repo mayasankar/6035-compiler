fb1dde5d24794412ff3b8e15b076854ae4ddb5d7
Jackie Bredenberg <jamb@mit.edu>
2017-10-11 17:15:18 -0400

    Decl + Statement

--- src/edu/mit/compilers/cfg/CFGDecl.java
+++ src/edu/mit/compilers/cfg/CFGDecl.java
@@ -1,6 +1,14 @@
 package edu.mit.compilers.cfg;
 import edu.mit.compilers.cfg.CFGLine;
+import edu.mit.compilers.cfg.decl.IRMemberDecl;
 
 public class CFGDecl extends CFGLine {
+    IRMemberDecl decl;
 
+    public CFGStatement(CFGLine trueBranch, CFGLine falseBranch, IRMemberDecl decl) {
+        super(trueBranch, falseBranch);
+        this.decl = decl;
+    }
+
+    public IRStatement getDecl() { return decl; }
 }
--- src/edu/mit/compilers/cfg/CFGStatement.java
+++ src/edu/mit/compilers/cfg/CFGStatement.java
@@ -1,6 +1,14 @@
 package edu.mit.compilers.cfg;
 import edu.mit.compilers.cfg.CFGLine;
+import edu.mit.compilers.cfg.statement.IRStatement;
 
 public class CFGStatement extends CFGLine {
     IRStatement statement;
+
+    public CFGStatement(CFGLine trueBranch, CFGLine falseBranch, IRStatement statement) {
+        super(trueBranch, falseBranch);
+        this.statement = statement;
+    }
+
+    public IRStatement getStatement() { return statement; }
 }
