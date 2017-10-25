7f3fb1c4160324ec42f2d4efafad4fc2fe040693
Jackie Bredenberg <jamb@mit.edu>
2017-10-25 14:18:25 -0400

    Oops push fail

---
+++ src/edu/mit/compilers/cfg/CFGLoopEnv.java
@@ -0,0 +1,37 @@
+package edu.mit.compilers.cfg;
+
+import java.io.OutputStream;
+import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+import antlr.Token;
+import edu.mit.compilers.ir.*;
+import edu.mit.compilers.ir.decl.*;
+import edu.mit.compilers.ir.expression.*;
+import edu.mit.compilers.ir.expression.literal.*;
+import edu.mit.compilers.ir.operator.*;
+import edu.mit.compilers.ir.statement.*;
+import edu.mit.compilers.symbol_tables.*;
+import edu.mit.compilers.trees.EnvStack;
+import edu.mit.compilers.cfg.*;
+
+
+public class CFGLoopEnv {
+
+    CFGLine startLine;  // where we go to after continue
+    CFGLine followingLine;  // where we go to after break
+
+    public CFGLoopEnv(CFGLine start, CFGLine end) {
+        startLine = start;
+        followingLine = end;
+    }
+
+    public CFGLine getStartLine() { return startLine; }
+    public CFGLine getFollowingLine() { return followingLine; }
+    public void setStartLine(CFGLine start) { startLine = start; }
+    public void setFollowingLine(CFGLine follow) { followingLine = follow; }
+
+}
