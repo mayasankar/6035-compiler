c82a7159e70913977cd57508ee1087743634aba9
mayasankar <mayasankar@gmail.com>
2017-11-14 15:54:18 -0500

    Added CP skeleton for reals

---
+++ src/edu/mit/compilers/cfg/optimizations/CP.java
@@ -0,0 +1,36 @@
+package edu.mit.compilers.cfg.optimizations;
+
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.mit.compilers.ir.*;
+import edu.mit.compilers.ir.decl.*;
+import edu.mit.compilers.ir.expression.*;
+import edu.mit.compilers.ir.expression.literal.*;
+import edu.mit.compilers.ir.statement.*;
+import edu.mit.compilers.symbol_tables.*;
+import edu.mit.compilers.trees.EnvStack;
+import edu.mit.compilers.cfg.*;
+import edu.mit.compilers.cfg.lines.*;
+
+public class CP implements Optimization {
+    public void optimize(CFGProgram cfgProgram) {
+        for (Map.Entry<String, CFG> method : cfgProgram.getMethodToCFGMap().entrySet()) {
+            CFG cfg = method.getValue();
+            doReachingDefinitionsAnalysis(cfg);
+            propagate(cfg);
+        }
+    }
+
+    private void doReachingDefinitionsAnalysis(CFG cfg) {
+
+    }
+
+    private void propagate(CFG cfg) {
+
+    }
+
+}
