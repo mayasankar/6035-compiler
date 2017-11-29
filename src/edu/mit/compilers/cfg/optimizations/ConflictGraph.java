bd6e3de2aa4466a2103c8fd104296401665b17c1
Jackie Bredenberg <jamb@mit.edu>
2017-11-28 22:18:14 -0500

    Coloring algorithm

--- src/edu/mit/compilers/cfg/optimizations/ConflictGraph.java
+++ src/edu/mit/compilers/cfg/optimizations/ConflictGraph.java
@@ -1,11 +1,13 @@
 package edu.mit.compilers.cfg.optimizations;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashSet;
 import java.util.Set;
 import java.math.BigInteger;
+import java.lang.Integer;
 
 import edu.mit.compilers.ir.*;
 import edu.mit.compilers.ir.decl.*;
@@ -47,6 +49,8 @@ public class ConflictGraph {
         var2Conflicts.remove(var1);
     }
 
+    public Set<String> getVariables() { return variableConflicts.keySet(); }
+
     public Set<String> getConflictingVariables(String variable) {
         Set<String> result = variableConflicts.get(variable);
         if (result == null) {
@@ -57,7 +61,49 @@ public class ConflictGraph {
 
     // assigns an integer to each variable such that there are minimal distinct integers; returns this mapping
     public Map<String, Integer> colorGraph() {
-        throw new RuntimeException("Unimplemented.");
+        Set<String> variables = getVariables();
+        List<String> varStack = new ArrayList<>();
+        List<Set<String>> conflictStack = new ArrayList<>();
+
+        // go through and always remove the thing with fewest connections, making a stack of vars/conflicts
+        while (variables.size() > 0) {
+            int minColors = Integer.MAX_VALUE;
+            String minColoredVar = null;
+            for (String var : variables) {
+                int colors = this.getConflictingVariables(var).size();
+                if (colors < minColors) {
+                    minColors = colors;
+                    minColoredVar = var;
+                }
+            }
+            varStack.add(minColoredVar);
+            conflictStack.add(this.getConflictingVariables(minColoredVar));
+            removeVariable(minColoredVar);
+            variables = getVariables();
+        }
+
+        // re-add the variables and color MethodAssembler
+        Map<String, Integer> coloring = new HashMap<>();
+        while (varStack.size() > 0) {
+            String var = varStack.remove(varStack.size()-1);
+            Set<String> conflicts = conflictStack.remove(conflictStack.size()-1);
+            Set<Integer> colorConflicts = new HashSet<>();
+            for (String conf: conflicts) {
+                if (!coloring.containsKey(conf)) {
+                    throw new RuntimeException("Variable being colored has an earlier neighbor uncolored. This should never happen.");
+                }
+                colorConflicts.add(coloring.get(conf));
+            }
+            int i = 0;  // set i to be the smallest unused color (greedy)
+            while (true) {
+                if (!colorConflicts.contains(i)) {
+                    break;
+                }
+                i++;
+            }
+            coloring.put(var, i);
+        }
+        return coloring;
     }
 
 }
