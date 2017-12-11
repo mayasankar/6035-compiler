2ee13ec2f79f42d838b9879d024c71f7a51e8daa
Jackie Bredenberg <jamb@mit.edu>
2017-12-11 13:15:52 -0500

    addConflict now adds the variables if they don't exist

--- src/edu/mit/compilers/cfg/optimizations/ConflictGraph.java
+++ src/edu/mit/compilers/cfg/optimizations/ConflictGraph.java
@@ -41,10 +41,21 @@ public class ConflictGraph {
     }
 
     public void addConflict(String var1, String var2) {
-        Set<String> var1Conflicts = getConflictingVariables(var1);
-        Set<String> var2Conflicts = getConflictingVariables(var2);
-        var1Conflicts.add(var2);
-        var2Conflicts.add(var1);
+        Set<String> var1Conflicts = variableConflicts.get(var1);
+        Set<String> var2Conflicts = variableConflicts.get(var2);
+        if (var1Conflicts == null) {
+            variableConflicts.put(var1, new HashSet<String>(Arrays.asList(var2)));
+        }
+        else {
+            var1Conflicts.add(var2);
+        }
+
+        if (var1Conflicts == null) {
+            variableConflicts.put(var2, new HashSet<String>(Arrays.asList(var1)));
+        }
+        else {
+            var2Conflicts.add(var1);
+        }
     }
 
     public void removeConflict(String var1, String var2) {
