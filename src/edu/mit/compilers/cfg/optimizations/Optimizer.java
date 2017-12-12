356730bd4b071f1d89f62e28203568c46e34f16f
Jackie Bredenberg <jamb@mit.edu>
2017-12-12 06:49:40 -0500

    reruns optimizations

--- src/edu/mit/compilers/cfg/optimizations/Optimizer.java
+++ src/edu/mit/compilers/cfg/optimizations/Optimizer.java
@@ -10,11 +10,25 @@ public class Optimizer {
     public final static Optimization[] optimizations = new Optimization[] { new Inline(), new CSE(), new CP(), new DCE(), new DeadVariableRemover(), new RegisterAllocation(), new CodeSimplifier() };
 
     public static void optimize(CFGProgram cfgProgram, boolean[] opts, boolean debug) {
-        // TODO make it so that the cycle of optimizations is run until no more optimizing possible.
-        for (int i = 0; i < opts.length; ++i) {
-            if (! opts[i] || i == 2) { continue; }
-            System.out.println(optimizationNames[i]);
-			optimizations[i].optimize(cfgProgram, debug);
+        // IL
+        if (opts[0]) {
+            optimizations[0].optimize(cfgProgram, debug);
         }
+        // CSE, CP, (DCE then DVR) looping until no changes
+        boolean anyChanges = true;
+        while (anyChanges) {
+            anyChanges = false;
+            for (int i = 1; i <= 4; i++) {
+                anyChanges = anyChanges || optimizations[i].optimize(cfgProgram, debug);
+            }
+        }
+        // RA, AS
+        if (opts[5]) {
+            optimizations[5].optimize(cfgProgram, debug);
+        }
+        if (opts[6]) {
+            optimizations[6].optimize(cfgProgram, debug);
+        }
+
     }
 }
